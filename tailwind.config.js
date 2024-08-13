f45f9afeac75bb07d432f278e3971f28a5e04114
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-08-13 11:22:48 +0600

    ready for work

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}', './style.css'],
+  theme: {
+    extend: {
+      colors: {
+        'pink-custom': '#FF42A5FF',
+      },
+    },
+  },
+  plugins: [],
+}
